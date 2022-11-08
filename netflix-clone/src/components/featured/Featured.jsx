0d8dbf40c4174cf92cc86e493c32bbca4655c367
thuotracy <tracyjacobs775@gmail.com>
2022-11-08 17:41:55 +0300

    added genre section

--- netflix-clone/src/components/featured/Featured.jsx
+++ netflix-clone/src/components/featured/Featured.jsx
@@ -9,6 +9,19 @@ const Featured = ({type}) => {
                 <span>{type === "movie" ? "Movies" : "Series"}</span>
                 <select name="genre" id="genre">
                     <option>Genre</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="crime">Crime</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="historical">Historical</option>
+                    <option value="horror">Horror</option>
+                    <option value="romance">Romance</option>
+                    <option value="sci-fi">Sci-fi</option>
+                    <option value="thriller">Thriller</option>
+                    <option value="western">Western</option>
+                    <option value="animation">Animation</option>
+                    <option value="drama">Drama</option>
+                    <option value="documentary">Documentary</option>
                 </select>
             </div>
         )}
