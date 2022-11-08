bff8241afc77050f63cd5ba8329f87847f5d55d8
thuotracy <tracyjacobs775@gmail.com>
2022-11-08 13:35:00 +0300

    created the home page

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import Home from './home/Home';
+import './App.scss';
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ netflix-clone/src/home/Home.jsx
@@ -0,0 +1,10 @@
+import { AcUnit } from '@material-ui/icons';
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className='home'></div>
+  )
+}
+
+export default Home
