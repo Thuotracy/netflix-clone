40f20cb2239781f62df702db1381d274da649f41
thuotracy <tracyjacobs775@gmail.com>
2022-11-08 15:18:37 +0300

    styled the navbar

--- netflix-clone/src/components/navbar/Navbar.jsx
+++ netflix-clone/src/components/navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import "./navbar.scss";
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
+import "./navbar.scss";
+
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img
@@ -15,20 +23,22 @@ const Navbar = () => {
           <span>New and Popular</span>
           <span>My List</span>
         </div>
-
         <div className="right">
-            <Search className="icon"/>
-            <span>Tracy</span>
-            <Notifications className="icon"/>
-            <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&drp=2&w=500" alt="" />
-            <div className="profile">
-                <ArrowDropDown className="icon"/>
-                <div className="options">
-                    <span>Settings</span>
-                    <span>LogOut</span>
-                </div>
-            </div>   
-        </div>   
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- netflix-clone/src/home/Home.jsx
+++ netflix-clone/src/home/Home.jsx
@@ -5,6 +5,8 @@ const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&drp=2&w=500" alt="" />
+
     </div>
   )
 }
