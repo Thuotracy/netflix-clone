2bc86935cc734e28ff5592478a4b87b40b5d8c33
thuotracy <tracyjacobs775@gmail.com>
2022-11-08 17:30:08 +0300

    styled the buttons

--- netflix-clone/src/components/featured/Featured.jsx
+++ netflix-clone/src/components/featured/Featured.jsx
@@ -7,7 +7,7 @@ const Featured = () => {
         <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
 
         <div className='info'>
-            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.fanart.tv%2Ffanart%2Fdark-5a2a7712af6d1.jpg&imgrefurl=https%3A%2F%2Ffanart.tv%2Fseries%2F334824%2Fdark%2F&tbnid=Z78R0ztGhmfKTM&vet=12ahUKEwiB4rra3Z77AhVAVaQEHbqOD-4QMygBegUIARDBAQ..i&docid=dS81MNldYr36_M&w=1000&h=562&q=dark%20series%20thumbnail&ved=2ahUKEwiB4rra3Z77AhVAVaQEHbqOD-4QMygBegUIARDBAQ" alt="" />
+            <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1" alt="" />
             <span className='desc'>
                 Lorem ipsum, in graphical and textual context, refers to filler text that is placed in a document or visual presentation. Lorem ipsum is derived from the Latin "dolorem ipsum" roughly translated as "pain itself."
             </span>
