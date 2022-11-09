33632e2487b00e4606a3f1ee4c1ae2318d009509
thuotracy <tracyjacobs775@gmail.com>
2022-11-09 13:29:44 +0300

    created the scroll functionality

--- netflix-clone/src/components/list/List.jsx
+++ netflix-clone/src/components/list/List.jsx
@@ -1,21 +1,34 @@
 import ListItem from '../listItem/ListItem'
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons'
+import { useRef, useState } from 'react'
 import './list.scss'
 
 export default function List() {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const listRef = useRef()
+
+  const handleClick = (direction) => {
+    setIsMoved(true)
+    let distance = listRef.current.getBoundingClientRect().x - 50
+    if(direction === 'left' && slideNumber > 0){
+      setSlideNumber(slideNumber - 1);
+        listRef.current.style.transform = `translateX(${235 + distance}px)`
+    }
+
+    if(direction === 'right' && slideNumber < 5){
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-235 + distance}px)`
+  }
+  }
+
   return (
     <div className='list'>
       <span className='listTitle'>Continue to watch</span>
       <div className='wrapper'>
-        <ArrowBackIosOutlined className='slideArrow left'/>
-          <div className='container'>
-            <ListItem/>
-            <ListItem/>
-            <ListItem/>
-            <ListItem/>
-            <ListItem/>
-            <ListItem/>
-            <ListItem/>
+        <ArrowBackIosOutlined className='slideArrow left' onClick={() => handleClick("left")} style={{display: !isMoved && 'none'}}/>
+          <div className='container' ref={listRef}>
             <ListItem/>
             <ListItem/>
             <ListItem/>
@@ -27,7 +40,7 @@ export default function List() {
             <ListItem/>
             <ListItem/>
           </div>
-        <ArrowForwardIosOutlined className='slideArrow right'/>
+        <ArrowForwardIosOutlined className='slideArrow right' onClick={() => handleClick("right")}/>
       </div>
     </div>
   )
