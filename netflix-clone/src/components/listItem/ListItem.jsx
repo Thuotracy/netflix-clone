072d9dd4ffc8b2bc1c930dcbd9208e8b33b004ef
thuotracy <tracyjacobs775@gmail.com>
2022-11-09 15:49:28 +0300

    added trailer

--- netflix-clone/src/components/listItem/ListItem.jsx
+++ netflix-clone/src/components/listItem/ListItem.jsx
@@ -1,39 +1,50 @@
-import { Add, PlayArrow, ThumbDownOutlined, ThumbUpAltOutlined } from '@material-ui/icons'
-import { useState } from 'react'
-import './listItem.scss'
+import {Add, PlayArrow, ThumbDownOutlined, ThumbUpAltOutlined,} from "@material-ui/icons";
+import { useState } from "react";
+import "./listItem.scss";
 
-export default function ListItem({index}) {
-
+export default function ListItem({ index }) {
   const [isHovered, setIsHovered] = useState(false);
 
+  const trailer ="https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
   return (
-    <div className='listitem' 
-      style={{left: isHovered && index * 225 - 50}}
-      onMouseEnter={() => setIsHovered(true)} 
-      onMouseLeave={() => setIsHovered(false)}>
-        <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt="" />
+    <div
+      className="listitem"
+      style={{ left: isHovered && index * 225 - 50 + index * 5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt=""/>
 
-        <div className='iteminfo'>
-          <div className='icons'>
-            <PlayArrow/>
-            <Add/>
-            <ThumbUpAltOutlined/>
-            <ThumbDownOutlined/>
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
 
-            <div className='itemInfoTop'>
-              <span>1 hour 14 mins</span>
-              <span className='limit'>+16</span>
-              <span>1999</span>
+          <div className="iteminfo">
+            <div className="icons">
+              <PlayArrow />
+              <Add />
+              <ThumbUpAltOutlined />
+              <ThumbDownOutlined />
+
+              <div className="itemInfoTop">
+                <span>1 hour 14 mins</span>
+                <span className="limit">+16</span>
+                <span>1999</span>
+              </div>
+
+              <div className="desc">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad,
+                mollitia at possimus est temporibus illum autem cupiditate
+                necessitatibus amet atque? Nesciunt voluptates accusamus
+                laboriosam. Maxime beatae sit a architecto doloremque.
+              </div>
+
+              <div className="genre">Action</div>
             </div>
-
-            <div className='description'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, mollitia at possimus est temporibus illum autem cupiditate necessitatibus amet atque? Nesciunt voluptates accusamus laboriosam. Maxime beatae sit a architecto doloremque.
-            </div>
-
-            <div className='genre'>Action</div>
-
           </div>
-        </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
