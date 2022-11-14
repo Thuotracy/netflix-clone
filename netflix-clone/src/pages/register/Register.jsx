9826a2852c06f9a37e29d6460f00c9827b700a0f
thuotracy <tracyjacobs775@gmail.com>
2022-11-14 17:44:45 +0300

    added the netflix logo

---
+++ netflix-clone/src/pages/register/Register.jsx
@@ -0,0 +1,12 @@
+import './>register.scss'
+
+export default function Register() {
+  return (
+    <div className='register'>
+        <div className='top'>
+            <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            <button className='loginButton'></button>
+        </div>
+    </div>
+  )
+}
