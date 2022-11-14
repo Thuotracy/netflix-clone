a2de1001d39b53921ce3b4208f666db36dcd24b5
thuotracy <tracyjacobs775@gmail.com>
2022-11-14 19:26:51 +0300

    created a useState

--- netflix-clone/src/pages/register/Register.jsx
+++ netflix-clone/src/pages/register/Register.jsx
@@ -1,6 +1,23 @@
-import './register.scss'
+import './register.scss';
+import { useRef } from 'react';
+import { useState } from 'react';
 
 export default function Register() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const emailRef = useRef
+    const passwordRef = useRef
+
+
+    const handleStart = () => {
+        setEmail(emailRef.current.value)
+    };
+    const handleFinish = () => {
+        setPassword(passwordRef.current.value);
+    };
+
   return (
     <div className='register'>
         <div className='top'>
@@ -14,10 +31,21 @@ export default function Register() {
             <h1>Unlimited movies, TV shows and more.</h1>
             <h2>Watch anywhere, Cancel anytime.</h2>
             <p>Ready to watch? Enter your Email to create or restart your membership</p>
-            <div className='input'>
-                <input type="email" placeholder='email address' />
-                <button className='registerButton'>Get started</button>
-            </div>
+                 {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
         </div>
     </div>
   )
