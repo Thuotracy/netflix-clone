807d9edccb392cd2a8d62eb2347cda0423c19c4d
thuotracy <tracyjacobs775@gmail.com>
2022-11-14 17:47:23 +0300

    made changes

--- netflix-clone/src/pages/register/Register.jsx
+++ netflix-clone/src/pages/register/Register.jsx
@@ -5,7 +5,13 @@ export default function Register() {
     <div className='register'>
         <div className='top'>
             <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-            <button className='loginButton'></button>
+            <button className='loginButton'>Sign In</button>
+        </div>
+
+        <div className='container'>
+            <h1>Unlimited movies, TV shows and more.</h1>
+            <h2>Watch anywhere, Cancel anytime.</h2>
+            <p>Ready to watch? Enter your Email to create or restart your membership</p>
         </div>
     </div>
   )
